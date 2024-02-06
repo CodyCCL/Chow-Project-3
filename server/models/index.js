85816004fa4dd45f7f98428752d23dc138a71ee7
kgarcia7 <kgarcia7814@gmail.com>
2024-02-05 20:28:57 -0500

    Adding index.js

---
+++ server/models/index.js
@@ -0,0 +1,12 @@
+const Category = require("./Category");
+const Ingredients = require("./Ingredients");
+const Macros = require("./Macros");
+const Meal = require("./Meal");
+const Order = require("./Order");
+const Restaurant = require("./Restaurant");
+const Review = require("./Review");
+const User = require("./User");
+
+module.exports = { Category, Ingredients, Macros, Meal, Order, Restaurant, Review, User };
+
+
