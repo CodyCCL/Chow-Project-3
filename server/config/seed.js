a74ed09503a985f6c2e991b272c476776b3817c9
kgarcia7 <kgarcia7814@gmail.com>
2024-02-05 21:31:58 -0500

    Update seed.js models

--- server/config/seed.js
+++ server/config/seed.js
@@ -14,7 +14,14 @@ const cleanDB = require('./cleanDB');
 db.once('open', async () => {
     //used to drop collections if they currently exist
     //caution will erase all existing data in these collections!!
-    await cleanDB('','');
+    await cleanDB("Category","categories");
+    await cleanDB("Ingredients", "ingredients");
+    await cleanDB("Macros", "macros");
+    await cleanDB("Meal", "meals");
+    await cleanDB("Order", "orders");
+    await cleanDB("Restaurant", "restaurant");
+    await cleanDB("Review", "reviews");
+    await cleanDB("User", "users");
 
     // Our seed source for our data models....
 
