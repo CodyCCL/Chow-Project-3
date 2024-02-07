a5b9da2f517ffbb7bcd187d6998ed7104637ca61
Cody Lin <chengchihlin1@gmail.com>
2024-02-06 21:23:41 -0500

    update clientside code

---
+++ client/src/pages/Saved.jsx
@@ -0,0 +1,12 @@
+//import { useState, useEffect } from 'react';
+//import Css
+//import { getMe, deletefood } from '../utils/API';
+//import Auth from '../utils/auth';
+//import { removeFoodId } from '../utils/localStorage';
+
+
+
+  //Todo: create function that accepts the book's mongo _id value as param and deletes the food from the database
+
+
+//export default Saved;
