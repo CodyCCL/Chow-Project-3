8bf822dc7f84fe12fc9490926755f27cb189fd0d
Joseph Browning <j.browning527@gmail.com>
2024-02-06 19:04:29 -0500

    started setting up resolvers and typdefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,8 +7,53 @@ const resolvers = {
         categories: async () => {
             return await Category.find();
           },
+          user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                path: 'orders.products',
+                populate: 'category'
+              });
+      
+              user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw AuthenticationError;
+          },
     },
-    Mutation: {}
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+          updateUser: async (parent, args, context) => {
+            if (context.user) {
+              return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+      
+            throw AuthenticationError;
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          }
+    }
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,32 @@
-const typeDefs = '';
+const typeDefs = `
+type Category {
+    _id: ID
+    name: String
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    user: User
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    login(email: String!, password: String!): Auth
+  }
+  `;
 
 module.exports = typeDefs;
