77f346791a21598969b916f9715ad2a6b475898f
Joseph Browning <j.browning527@gmail.com>
2024-02-10 14:01:02 -0500

    fixed issues with frontend not being able to talk to graphql serverside.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 // layout
 import Layout from "./Layout";
 
@@ -13,9 +19,32 @@ import About from "./pages/About";
 import Food from "./pages/Food";
 import SignUp from "./components/SignUp";
 
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
+    console.log('GQL: '),
+    console.log(client),
     <Router>
+    <ApolloProvider client={client}>
       <Layout>
         <Routes>
           <Route exact path="/" element={<Home />} />
@@ -29,6 +58,7 @@ const App = () => {
           <Route path="/SignUp" element={<SignUp />} />
         </Routes>
       </Layout>
+      </ApolloProvider>
     </Router>
   );
 };
